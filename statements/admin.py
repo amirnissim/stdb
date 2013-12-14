0168958195968bbe7b573aabc37f429e7f991965
Amir Nissim <amirnissim@gmail.com>
2013-12-14 12:32:29 +0200

    models and fixtures

--- statements/admin.py
+++ statements/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from statements.models import Statement
+from statements.models import Statement, PubFig
 
 admin.site.register(Statement)
+admin.site.register(PubFig)
--- statements/models.py
+++ statements/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
+class PubFig(models.Model):
+  name = models.CharField(max_length=255)
+  image_url = models.URLField()
+
+  def __unicode__(self):
+    return self.name
+
+
 class Statement(models.Model):
+    pubfig = models.ForeignKey(PubFig)
     quote = models.TextField()
+    date = models.DateField()
+    about = models.CharField(max_length=255)
+    context = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    source_url = models.URLField(null=True, blank=True)
+
+    def __unicode__(self):
+      return self.quote[:10] + "..."
